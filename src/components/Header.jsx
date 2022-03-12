43cd05c0997d63fb81d7d7d4c4ce6f0ef7c0ad72
ar-arif <just.ar.arif@gmail.com>
2022-03-12 13:18:44 +0600

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -61,7 +61,7 @@ export default function Header(props) {
           type,
           data: resData,
         };
-        localStorage.setItem("lastID", data);
+        localStorage.setItem("lastID", JSON.stringify(data));
       });
       catch(err){
         console.error(err)
