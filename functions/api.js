78f0fe55dd76500910426be43ec6d5d4b5573ea3
ar-arif <just.ar.arif@gmail.com>
2022-03-12 12:52:41 +0600

    random express

--- functions/api.js
+++ functions/api.js
@@ -2,22 +2,26 @@ const { Client } = require("youtubei");
 const youtube = new Client();
 
 exports.handler = async function (event) {
-	const id = event.queryStringParameters.id;
-	if (id) {
-		if (id.includes("list")) {
-			const index = id.indexOf("=") + 1;
-			const pid = id.slice(index);
-			const playlist = await youtube.getPlaylist(pid);
-			return {
-				statusCode: 200,
-				body: JSON.stringify(playlist),
-			};
-		} else {
-			const video = await youtube.getVideo(id);
-			return {
-				statusCode: 200,
-				body: JSON.stringify(video),
-			};
+	try {
+		const id = event.queryStringParameters.id;
+		if (id) {
+			if (id.includes("list")) {
+				const index = id.indexOf("=") + 1;
+				const pid = id.slice(index);
+				const playlist = await youtube.getPlaylist(pid);
+				return {
+					statusCode: 200,
+					body: JSON.stringify(playlist),
+				};
+			} else {
+				const video = await youtube.getVideo(id);
+				return {
+					statusCode: 200,
+					body: JSON.stringify(video),
+				};
+			}
 		}
+	} catch (error) {
+		return { statusCode: 500, body: error.toString() };
 	}
 };
