2df4d37a9855ffe049c88c649c9f90268309818b
ar-arif <just.ar.arif@gmail.com>
2022-03-15 13:34:22 +0600

    pwa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,38 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      mode: "development",
+      base: "/",
+      includeAssets: ["favicon.svg"],
+      manifest: {
+        name: "DF YouTube",
+        short_name: "shortName",
+        theme_color: "#f44336",
+        icons: [
+          {
+            src: "pwa-192x192.png", // <== don't add slash, for testing
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png", // <== don't remove slash, for testing
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png", // <== don't add slash, for testing
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
